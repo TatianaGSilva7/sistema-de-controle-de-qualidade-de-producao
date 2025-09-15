140536ed102f7bed00c920326037927a8d0d7962
nicsant47-hash <nicsant47@gmail.com>
2025-09-15 19:16:06 -0300

    atualizado2

--- controleDeQualidade/src/entity/ProdutoAlimenticio.java
+++ controleDeQualidade/src/entity/ProdutoAlimenticio.java
@@ -1,6 +1,43 @@
 package entity;
 
-public class ProdutoAlimenticio {
-    private 
+public class ProdutoAlimenticio extends Produto {
+    private  String dataValidade ;
+    private  Boolean embalagemIntegra ;
+
+
+    
+    public ProdutoAlimenticio() {
+    }
+
+
+
+    public ProdutoAlimenticio(String dataValidade, Boolean embalagemIntegra) {
+        this.dataValidade = dataValidade;
+        this.embalagemIntegra = embalagemIntegra;
+    }
+
+
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+
+
+    public Boolean getEmbalagemIntegra() {
+        return embalagemIntegra;
+    }
+
+
+
+    public void setEmbalagemIntegra(Boolean embalagemIntegra) {
+        this.embalagemIntegra = embalagemIntegra;
+    }
     
 }
--- controleDeQualidade/src/entity/ProdutoPerecivel.java
+++ controleDeQualidade/src/entity/ProdutoPerecivel.java
@@ -1,6 +1,6 @@
 package entity;
 
-public class ProdutoPerecivel {
+public class ProdutoPerecivel  extends ProdutoAlimenticio{
     private String temperaturaArmazenamento;
     private String temperatuaMax;
     private String temperaturaMin;
@@ -39,6 +39,12 @@ public class ProdutoPerecivel {
         this.temperaturaMin = temperaturaMin;
     }
 
-    
+    @Override
+public String toString() {
+    return super.toString() +
+           ", Temp. Armazenamento: " + temperaturaArmazenamento +
+           ", Temp. Máx: " + temperatuaMax +
+           ", Temp. Mín: " + temperaturaMin;
+}
     
 }
